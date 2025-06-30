2cf9e6dac968f8f4ddb27bfec536a0bd416f81a4
saichitale-27 <sairc123456@gmail.com>
2025-06-30 22:44:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+app = Flask(__name__)
+data = pd.read_csv('stock_prices.csv')
+data['Date'] = pd.to_datetime(data['Date'])
+data['Days'] = (data['Date'] - data['Date'].min()).dt.days
+X = data[['Days']]
+y = data['Close']
+
+model = LinearRegression()
+model.fit(X, y)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    prediction = None
+    if request.method == 'POST':
+        days_ahead = int(request.form['days'])
+        if days_ahead < 0:
+            prediction = 'Days cannot be negative. Please enter a positive number.'
+        else:
+            future_day = np.array([[data['Days'].max() + days_ahead]])
+            prediction = model.predict(future_day)[0]
+    return render_template('index.html', prediction=prediction)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
